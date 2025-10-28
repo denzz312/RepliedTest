485ec59e0eaf64e0ef935ad0645c99ee14934341
Deniz <denzz312312@gmail.com>
2025-10-28 19:26:39 -0400

    feat: GH-1 configure initial setup

---
+++ src/main/java/org/example/repliedtest/RepliedTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.repliedtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepliedTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepliedTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/repliedtest/RepliedTestApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.repliedtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = RepliedTestApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class RepliedTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // TODO: Add tests
+    }
+
+}
